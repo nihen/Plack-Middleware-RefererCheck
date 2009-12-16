8cfb097fdb779ca6fdd6a3a50a251685e86493be
Masahiro Chiba <chiba@geminium.com>
2009-12-16 15:39:14 +0900

    initial import

---
+++ lib/Plack/Middleware/RefererCheck.pm
@@ -0,0 +1,75 @@
+package Plack::Middleware::RefererCheck;
+
+use strict;
+use 5.008_001;
+use parent qw(Plack::Middleware);
+ 
+__PACKAGE__->mk_accessors(qw(host same_scheme error_app));
+ 
+our $VERSION = '0.01';
+
+sub call {
+    my($self, $env) = @_;
+
+    $self->_check($env) ? $self->app->($env) : $self->error_app ? $self->error_app->($env) : _default_error_app();
+}
+
+sub _check {
+    my ( $self, $env ) = @_;
+
+    return 1 if $env->{REQUEST_METHOD} ne 'POST';
+
+    my $scheme = $self->same_scheme ? qr{\Q$env->{'psgi.url_scheme'}\E} : qr{https?};
+    my $host = $self->host || $env->{HTTP_HOST};
+        $host = qr{\Q$host\E};
+
+    return $env->{HTTP_REFERER} =~ m{\A$scheme://$host(?:/|\Z)};
+}
+
+sub _default_error_app {
+    return ['403', ['Content-Type' => 'text/plain', 'Content-Length' => 9], ['Forbidden']];
+}
+ 
+1;
+ 
+__END__
+ 
+=head1 NAME
+ 
+Plack::Middleware::RefererCheck - check referer for defensive CSRF attack.
+ 
+=head1 SYNOPSIS
+ 
+  use Plack::Builder;
+
+  builder {
+      enable 'RefererCheck', host => 'www.example.com', same_scheme => 1;
+      $app;
+  };
+ 
+  or more simply(host from $env->{HTTP_HOST} and same_scheme => 0)
+  # this is vulnerabilly for DNS Rebinding
+  builder {
+      enable 'RefererCheck';
+      $app;
+  };
+
+
+=head1 DESCRIPTION
+ 
+Plack::Middleware::RefererCheck
+ 
+=head1 AUTHOR
+ 
+Masahiro Chiba
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+ 
+=head1 SEE ALSO
+ 
+L<Plack::Middleware> L<Plack::Builder>
+ 
+=cut
---
+++ t/lib/Plack/Test/MockHTTP/ThroughScheme.pm
@@ -0,0 +1,58 @@
+package Plack::Test::MockHTTP::ThroughScheme;
+use strict;
+use warnings;
+
+use Carp;
+use HTTP::Request;
+use HTTP::Response;
+use HTTP::Message::PSGI;
+use Try::Tiny;
+
+sub test_psgi {
+    my %args = @_;
+
+    my $client = delete $args{client} or croak "client test code needed";
+    my $app    = delete $args{app}    or croak "app needed";
+
+    my $cb = sub {
+        my $req = shift;
+        my $env = $req->to_psgi;
+        my $res;
+        try {
+            $res = HTTP::Response->from_psgi($app->($env));
+        } catch {
+            $res = HTTP::Response->new(500);
+            $res->content($_);
+        };
+        $res->request($req);
+        return $res;
+    };
+
+    $client->($cb);
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Plack::Test::MockHTTP::ThroughScheme - Run mocked HTTP tests through PSGI applications
+
+=head1 DESCRIPTION
+
+Plack::Test::MockHTTP is an utility to run PSGI application given
+HTTP::Request objects and return HTTP::Response object out of PSGI
+application response. See L<Plack::Test> how to use this module.
+
+=head1 AUTHOR
+
+Tatsuhiko Miyagawa
+
+=head1 SEE ALSO
+
+L<Plack::Test>
+
+=cut
+
+
